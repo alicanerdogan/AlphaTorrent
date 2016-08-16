3ee4fac17470912a722d219d62e00198d7a2d051
alicanerdogan <aerdogan07@gmail.com>
2016-08-17 00:20:29 +0300

    Added sub functionality to the torrentFile

--- src/FileOperations/torrentFile.js
+++ src/FileOperations/torrentFile.js
@@ -4,3 +4,65 @@ import fs from 'fs';
 export function read(torrentPath) {
   let fileBuffer = fs.readFileSync(torrentPath);
 }
+
+function readDictionary() {
+  // returns map
+}
+
+export function readInteger(buffer, index) {
+  if(byteToChar(buffer[index]) !== 'i')
+  {
+    throw new Error('Invalid buffer data: missing prefix \'i\'');
+  }
+  index++;
+
+  let integerAsString = '';
+
+  while (isNumber(buffer[index])) {
+    integerAsString += byteToChar(buffer[index]);
+    index++;
+  }
+
+  if (byteToChar(buffer[index]) !== 'e') {
+    throw new Error('Invalid buffer data: missing postfix \'e\'');
+  }
+
+  index++;
+
+  return parseInt(integerAsString);
+}
+
+function readKeyValuePair() {
+  // return array
+}
+
+export function readString(buffer, index) {
+  let lengthAsString = '';
+
+  while (isNumber(buffer[index])) {
+    lengthAsString += byteToChar(buffer[index]);
+    index++;
+  }
+
+  if (byteToChar(buffer[index]) !== ':') {
+    throw new Error('Invalid buffer data: missing semicolon in string');
+  }
+
+  index++;
+
+  let length = parseInt(lengthAsString);
+
+  return buffer.slice(index, index + length).toString('ascii');
+}
+
+function readList() {
+  // return array
+}
+
+function byteToChar(byte) {
+  return String.fromCharCode(byte);
+}
+
+function isNumber(byte) {
+  return byte >= 48 && byte <= 57;
+}
--- src/FileOperations/torrentFile.spec.js
+++ src/FileOperations/torrentFile.spec.js
@@ -1,9 +1,34 @@
 import { read as readTorrentFile } from './torrentFile';
+import { readString } from './torrentFile';
+import { readInteger } from './torrentFile';
+import { expect } from 'chai';
 
 describe('TorrentFile', () => {
   describe('read operation', () => {
     it('should read torrent file', () => {
       readTorrentFile('./src/FileOperations/test/ubuntu.torrent');
+    });
+  });
+  describe('read string operation', () => {
+    it('should read raw string buffer', () => {
+      let testString = '8:announce';
+      let expected = 'announce';
+
+      let stringBuffer = Buffer.from(testString, 'ascii');
+      let result = readString(stringBuffer, 0);
+
+      expect(result).to.equal(expected);
+    });
+  });
+  describe('read integer operation', () => {
+    it('should read raw integer buffer', () => {
+      let testString = 'i59616e';
+      let expected = 59616;
+
+      let stringBuffer = Buffer.from(testString, 'ascii');
+      let result = readInteger(stringBuffer, 0);
+
+      expect(result).to.equal(expected);
     });
   });
 });

ec07b9a0e96b3e4e435302fbb75573fdb4a213f7
alicanerdogan <aerdogan07@gmail.com>
2016-10-16 15:32:20 +0300

    Implemented file writing for pieces

--- src/DataStructures/pieces.js
+++ src/DataStructures/pieces.js
@@ -1,12 +1,14 @@
 import EventEmitter from 'events';
+import fs from 'fs';
 
 export default class Pieces extends EventEmitter {
   constructor(totalSize, pieceSize) {
     super();
     this.pieceCount = Math.ceil(parseFloat(totalSize)/pieceSize);
     this.pieces = [];
-    console.log(`Total Size: ${totalSize}`);
-    console.log(`Piece count: ${this.pieceCount}`);
+    this.pieceSize = pieceSize;
+    this.totalSize = totalSize;
+    this.completed = 0;
   }
 
   add(index, piece) {
@@ -14,12 +16,45 @@ export default class Pieces extends EventEmitter {
       throw new Error(`Invalid piece index: ${index}`);
     }
     this.pieces[index] = piece;
+    this.completed++;
     if (this.isCompleted()) {
+      console.log('COMPLETED PIECES!');
       this.emit('completed');
     }
+    console.log((`Completed piece count: ${this.completed}/${this.pieceCount} Completed piece index: ${index}`));
   }
 
   isCompleted() {
-    return this.pieces.length === this.pieceCount;
+    return this.completed === this.pieceCount;
+  }
+
+  writeToFile(filename, startIndex, length) {
+    const fileStream = fs.createWriteStream(filename);
+    
+    const startPiece = Math.floor(startIndex/this.pieceSize);
+    const endPiece = Math.floor((startIndex+length)/this.pieceSize);
+    let remaining = length;
+    startIndex = startIndex % this.pieceSize;
+    fileStream.write(this.pieces[startPiece].buffer.slice(startIndex));
+    if(remaining < this.pieceSize) {
+      remaining = 0;
+    } else {
+      remaining -= (this.pieceSize - startIndex);
+    }  
+    for (var i = startPiece + 1; i <= endPiece; i++) {
+      if(remaining < this.pieceSize) {
+        fileStream.write(this.pieces[i].buffer, remaining);
+        remaining = 0;
+        break;
+      }
+      else {
+        fileStream.write(this.pieces[i].buffer);
+        remaining -= this.pieceSize;
+      }
+    }
+    fileStream.end();
+    if(remaining !== 0) {
+      throw new Error('Incomplete file');
+    }
   }
 }

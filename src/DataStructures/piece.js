54f7c342324e793f76264bc4cb59ad82ff40ae53
alicanerdogan <aerdogan07@gmail.com>
2016-10-16 15:31:38 +0300

    Implemented hash checking for piece

--- src/DataStructures/piece.js
+++ src/DataStructures/piece.js
@@ -1,10 +1,20 @@
+const crypto = require('crypto');
+
 export default class Piece {
-  constructor(size, hash) {
+  constructor(index, size, hash) {
+    this.index = index;
+    this.size = size;
     this.buffer = Buffer.alloc(size);
     this.hash = Buffer.from(hash);
   }
 
   update(startIndex, data) {
     this.buffer.fill(data, startIndex);
+  }
+
+  isIntact() {
+    const hash = crypto.createHash('sha1');
+    hash.update(this.buffer);
+    return this.hash.equals(hash.digest());
   }
 }

f51734f26dd9407d563661f7ca8c71fc45cb7f26
alicanerdogan <aerdogan07@gmail.com>
2016-10-16 15:29:42 +0300

    Extended peer functionality

--- src/Connection/peer.js
+++ src/Connection/peer.js
@@ -1,20 +1,77 @@
 import net from 'net';
+import EventEmitter from 'events';
+import { encodeHandshake } from './Messages/handshake';
+import { encodeInterested } from './Messages/interested';
+import parseMessage from './Messages/parseMessage';
 
-export default class Peer {
+export default class Peer extends EventEmitter {
   constructor(ip, port) {
+    super();
     this.ip = ip;
     this.port = parseInt(port);
+    this.isUnchoked = false;
   }
 
   connect() {
+    setTimeout(() => {
+      if (this.client.connecting) {
+        this.client.destroy();
+      }
+    }, 2000);
     return new Promise((resolve, reject) => {
       const options = {
         port: this.port,
         host: this.ip
       };
       this.client = net.connect(options, () => {
+        this.subscribeData((data) => {
+          const message = parseMessage(data);
+          if (message) {
+            if (message.type === 'unchoke') {
+              this.isUnchoked = true;
+              console.log(`${this.ip}:${this.port} is unchoked`);
+              this.emit('unchoked');
+            }
+            else if (message.type === 'choke') {
+              this.isUnchoked = false;
+              console.log(`${this.ip}:${this.port} is choked`);
+              this.emit('choked');
+            }
+          }
+        });
         resolve();
       });
+      this.client.once('error', (error) => {
+        reject(`connection error on ${error}@${this.ip}:${this.port}`);
+      });
+      this.client.once('close', (had_error) => {
+        reject(`connection closed on ${this.ip}:${this.port}`);
+      });
+    });
+  }
+
+  connectAndHandshake(infohash, id) {
+    return this.connect().then(() => {
+      console.log(`connected to ${this.ip}:${this.port}`);
+      const handshake = encodeHandshake(infohash, id);
+      const waitForHandshake = this.waitForResponse().then((data) => {
+        return new Promise((resolve, reject) => {
+          const message = parseMessage(data);
+          if (message.type === 'handshake') {
+            console.log(`handshaked with ${this.ip}:${this.port}`);
+            const interested = encodeInterested();
+            this.sendData(interested);
+            console.log('interest shown');
+            resolve();
+          }
+          else {
+            console.log('handshake is not received');
+            reject();
+          }
+        });
+      });
+      this.sendData(handshake);
+      return waitForHandshake;
     });
   }
 

73760dbe9baf03a1026e7e59afc3ff7193dfb255
alicanerdogan <aerdogan07@gmail.com>
2016-10-25 09:10:35 +0300

    Removed index paramater from  piece.add

--- src/DataStructures/pieces.js
+++ src/DataStructures/pieces.js
@@ -11,17 +11,17 @@ export default class Pieces extends EventEmitter {
     this.completed = 0;
   }
 
-  add(index, piece) {
-    if (index < 0 || index >= this.pieceCount) {
-      throw new Error(`Invalid piece index: ${index}`);
+  add(piece) {
+    if (piece.index < 0 || piece.index >= this.pieceCount) {
+      throw new Error(`Invalid piece index: ${piece.index}`);
     }
-    this.pieces[index] = piece;
+    this.pieces[piece.index] = piece;
     this.completed++;
     if (this.isCompleted()) {
       console.log('COMPLETED PIECES!');
       this.emit('completed');
     }
-    console.log((`Completed piece count: ${this.completed}/${this.pieceCount} Completed piece index: ${index}`));
+    console.log((`Completed piece count: ${this.completed}/${this.pieceCount} Completed piece index: ${piece.index}`));
   }
 
   isCompleted() {
